b04d2ff1acd0a545817d773b2ad1fe10e311670c
Solamish <1124053312@qq.com>
2020-05-07 23:36:29 +0800

    Update db.go

--- model/db.go
+++ model/db.go
@@ -12,7 +12,7 @@ var (
 
 func InitDB() {
  	var err error
-	DB,err = gorm.Open("mysql","root:zhy123@tcp(127.0.0.1:3306)/secKill?parseTime=true&charset=utf8&loc=Local")
+	DB,err = gorm.Open("mysql","root:@tcp(127.0.0.1:3306)/secKill?parseTime=true&charset=utf8&loc=Local")
 	if err != nil {
 		log.Println("Fail to open mysql :",err)
 		return
